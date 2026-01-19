30695ab3458b0ed8287619cfb7f67d823f25cb43
gokul <gokulkarunaharan41@gmail.com>
2026-01-19 06:02:37 +0530

    first commit

---
+++ src/main/java/com/gokul/ptpskillshare/PtpSkillShareApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.ptpskillshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtpSkillShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtpSkillShareApplication.class, args);
+    }
+
+}
